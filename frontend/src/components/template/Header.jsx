133dcc6834cc6465dee61783995da0ad2eb6a3ec
DavidIsrael <davidisrael.rj@gmail.com>
2023-07-31 10:36:39 -0300

    Ajustando o componente Rodapé

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -7,5 +7,5 @@ export default props =>
         <i className={`fa fa-${props.icon}`}></i>{props.title}
     </h1>
     <p className="lead text-muted">{props.subtitle}</p>
-    Cabeçalho
+    
 </header>
